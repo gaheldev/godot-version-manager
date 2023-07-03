9c353abe1a3ef0b8d2ffedcaecf481f0297a7551
zelv <contact@elv-studio.com>
2023-07-03 04:14:37 +0200

    Fix: desktop file is installed without requiring restart

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo cp godot-install /usr/local/bin/
 sudo chmod +x /usr/local/bin/godot-install
 sudo cp godot.png /usr/share/pixmaps/
 sudo cp godot.desktop /usr/share/applications/
+xdg-desktop-menu install --novendor /usr/share/applications/godot.desktop
