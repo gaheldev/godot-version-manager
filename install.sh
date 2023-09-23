a657ee185bfa12afea307c80d0f68ddce5923e2f
Gahel <music@gahel.fr>
2023-09-23 14:52:40 +0200

    completion: better location for bash completion file

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ pyinstaller -F main.py -n godot-version-manager
 
 sudo cp dist/godot-version-manager /usr/local/bin/
 register-python-argcomplete godot-version-manager > godot-version-manager.completion
-sudo cp godot-version-manager.completion /etc/bash_completion.d/godot-version-manager
+sudo cp godot-version-manager.completion /usr/share/bash-completion/completions/godot-version-manager
 
 sudo cp godot.png /usr/share/pixmaps/
 sudo cp godot.desktop /usr/share/applications/
