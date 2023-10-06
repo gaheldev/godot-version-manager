b28b4cebe861abd685ca31cb1fcb052901ff9b5e
Gahel <music@gahel.fr>
2023-10-06 02:34:52 +0200

    downloader: make relevant functions private

--- downloader.py
+++ downloader.py
@@ -40,19 +40,19 @@ ARCHIVE_REPO = 'https://downloads.tuxfamily.org/godotengine/'
 # matches 3.x, 4.x, 3.x.x.x ...
 regex_version_number = re.compile(r'[3-4](\.\d)+')
 
-def is_supported_version_number(s: str) -> bool:
+def _is_supported_version_number(s: str) -> bool:
     return regex_version_number.match(s) is not None
 
-def is_version_dir(dir_name: str) -> bool:
-    return is_supported_version_number(dir_name.rstrip('/'))
+def _is_version_dir(dir_name: str) -> bool:
+    return _is_supported_version_number(dir_name.rstrip('/'))
 
-def is_prerelease_dir(dir_name: str) -> bool:
+def _is_prerelease_dir(dir_name: str) -> bool:
     return ('rc' in dir_name)\
            or ('alpha' in dir_name)\
            or ('beta' in dir_name)\
            or ('dev' in dir_name)
 
-def is_app(href: str) -> bool:
+def _is_app(href: str) -> bool:
     return href.endswith('.zip')
 
 
@@ -63,7 +63,7 @@ def get_version_numbers() -> Generator[str, None, None]:
     root_dirs_tags = soup.select('table')[0].tbody.find_all('a')
     for tag in root_dirs_tags:
         root_dir = tag.get('href')
-        if is_version_dir(root_dir):
+        if _is_version_dir(root_dir):
             yield root_dir.rstrip('/')
 
 
@@ -82,7 +82,7 @@ def get_release_names(version: str) -> Generator[str, None, None]:
     yielded_stable = False
     for tag in links_tags:
         link = tag.get('href')
-        if is_prerelease_dir(link):
+        if _is_prerelease_dir(link):
             yield link.rstrip('/')
         if not yielded_stable:
             if 'stable' in link:
@@ -90,7 +90,7 @@ def get_release_names(version: str) -> Generator[str, None, None]:
                 yielded_stable = True
 
 
-def app_name_matches(name: str, system, architecture):
+def _app_name_matches(name: str, system, architecture):
     match system:
         case 'linux':
             if 'linux' not in name:
@@ -134,10 +134,10 @@ def download_app(version_number: str,
                  architecture='64',
                  mono=False,
                  release='stable') -> str:
-    app_links = get_app_links(version_number, mono=mono, release=release)
+    app_links = _get_app_links(version_number, mono=mono, release=release)
     matching_links = []
     for link in app_links:
-        if app_name_matches(link, system, architecture):
+        if _app_name_matches(link, system, architecture):
             matching_links.append(link)
 
     if len(matching_links) == 0:
@@ -154,7 +154,7 @@ def download_app(version_number: str,
 
 
 
-def get_app_links(version_number: str, mono=False, release='stable') -> list[str]:
+def _get_app_links(version_number: str, mono=False, release='stable') -> list[str]:
     repo = os.path.join(ARCHIVE_REPO, version_number)
     if release != 'stable':
         repo = os.path.join(repo, release)
@@ -166,7 +166,7 @@ def get_app_links(version_number: str, mono=False, release='stable') -> list[str
     # get all hyperlinks in the table, which include downloadable Godot apps
     hrefs_tags = soup.select('table')[0].tbody.find_all('a')
     hrefs = [tag.get('href') for tag in hrefs_tags]
-    app_links = [os.path.join(repo,href) for href in hrefs if is_app(href)]
+    app_links = [os.path.join(repo,href) for href in hrefs if _is_app(href)]
     return app_links
 
 
