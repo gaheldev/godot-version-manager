976015bc695585efc1d455f255a2ba8dd2b096c5
Gahel <music@gahel.fr>
2023-10-20 01:10:26 +0800

    refacto: use 'remote' as generic name in downloader

--- gdvm/downloader/downloader.py
+++ gdvm/downloader/downloader.py
@@ -1,6 +1,6 @@
 from typing import Generator
 
-from . import tuxfamily as tf
+from . import tuxfamily as remote
 from ..paths import TMP_DIR
 
 
@@ -8,15 +8,15 @@ from ..paths import TMP_DIR
 
 
 def get_version_numbers() -> Generator[str, None, None]:
-    return tf.get_version_numbers()
+    return remote.get_version_numbers()
 
 
 def get_prerelease_names(version: str) -> Generator[str, None, None]:
-    return tf.get_prerelease_names(version)
+    return remote.get_prerelease_names(version)
 
 
 def get_release_names(version: str) -> Generator[str, None, None]:
-    return tf.get_release_names(version)
+    return remote.get_release_names(version)
 
 
 def download_app(version_number: str,
@@ -26,9 +26,9 @@ def download_app(version_number: str,
                  release='stable',
                  dl_path=TMP_DIR) -> str:
 
-    return tf.download_app(version_number,
-                           dl_path,
-                           system=system,
-                           architecture=architecture,
-                           mono=mono,
-                           release=release)
+    return remote.download_app(version_number,
+                               dl_path,
+                               system=system,
+                               architecture=architecture,
+                               mono=mono,
+                               release=release)
