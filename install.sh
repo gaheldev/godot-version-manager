cce762edd770c657f146eac03745f35896ac56e5
Gahel <music@gahel.fr>
2023-10-23 12:04:21 +0800

    feat: create ~/.local/bin if it doesn't exist
    also add TODO about adding it to path if necessary

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+# TODO: add ~/.local/bin to path if it's not in it
+mkdir -p ~/.local/bin
+
 cp dist/gdvm ~/.local/bin/
+
 sudo cp gdvm.completion /usr/share/bash-completion/completions/gdvm
 
 sudo cp godot.png /usr/share/pixmaps/
