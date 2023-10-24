771f2f20426495851bdca44dafb91c9ed383213d
Gahel <music@gahel.fr>
2023-10-24 18:05:07 +0800

    cd: wip: trying to make release.sh work on github

--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
+echo "Creating release for linux..."
+
 system="linux"
 dir="release/${system}/gdvm"
 mkdir -p ${dir}/dist
@@ -7,10 +9,13 @@ cp gdvm.completion ${dir}/
 cp godot.png ${dir}/
 cp install.sh ${dir}/
 
+
 # TODO: simplify
 (
 cd ${dir}/..
 archive="gdvm_${system}.zip"
 zip -r ${archive} gdvm
 mv ${archive} ..
+
+echo "linux release created at release/$archive"
 )
