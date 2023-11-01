46a8ec4e1b158c06b614dca9218db6a6ef9fcef0
Gahel <music@gahel.fr>
2023-11-02 02:48:00 +0800

    wip: pseudo code for creating Godot app shortcut on mac

--- gdvm/desktop.py
+++ gdvm/desktop.py
@@ -26,6 +26,20 @@ Categories=Development;"""
 
                 f.write(linux_desktop)
                 sp.run(['xdg-desktop-menu', 'install', '--novendor', f'{DESKTOP_PATH}'])
+
+
+        case 'osx':
+            # TODO:
+            # create DESKTOP_PATH
+            # copy godot exe to INSTALL_PATH
+            # need to copy godot bin to ~/.local/bin/ too ?
+            # copy godot.icns to DESKTOP_PATH/Contents/Resources/  (store godot.icns in '~/Library/Application Support/gdvm/ ?)
+            # /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -f "DESKTOP_PATH"
+            raise Exception('desktop shortcut not implemented for osx')
+
+
+        case 'windows':
+            raise Exception('desktop shortcut not implemented for windows')
                 
 
 
