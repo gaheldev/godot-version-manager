34c73806e3dec03ec517b6f03e55d970ee2bf657
Firliminat <bobet.alexandre@gmail.com>
2024-03-13 22:50:23 +0100

    Implement windows pre-commit-hook script

--- scripts/windows/pre-commit-hook.ps1
+++ scripts/windows/pre-commit-hook.ps1
@@ -1,16 +1,19 @@
 #!pwsh
 
-echo "#!pwsh
+# Creating powershell pre-commit hookscript
+"#!pwsh
 
 # run tests
 pytest -v --online
 
 # pass the failure code back to the pre-commit hook
-exit $LASTEXITCODE
-" > .git/hooks/pre-commit.ps1
+exit `$LASTEXITCODE
+" > .git\hooks\pre-commit.ps1
 
-echo "#!/bin/sh
-echo 
-exec pwsh.exe -ExecutionPolicy RemoteSigned -File '.\pre-commit.ps1'
-exit
-" > .git/hooks/pre-commit
+# Creating shell wrapper
+$Wrapper = @"
+#!/usr/bin/env sh
+pwsh -File .git/hooks/pre-commit.ps1
+
+"@
+$Wrapper -replace "`r`n", "`n" | Out-File ".git/hooks/pre-commit" -Encoding utf8NoBOM -NoNewline
